5f01d94fa9e2059d95608040bd2d41a9d8f0e459
Saniyaiqbal06 <syedsaniyaiqbal2006@gmail.com>
2026-07-30 15:38:22 +0530

    Add files via upload

---
+++ EXP10/EXP10-D.R
@@ -0,0 +1,8 @@
+Student_ID <- c(101,102,103,104,105)
+Student_Name <- c("Rahul","Priya","Kiran","Sneha","Arjun")
+Marks <- c(85,90,78,88,95)
+
+student_data <- data.frame(Student_ID, Student_Name, Marks)
+
+cat("Student Marks Data Frame:\n")
+print(student_data)
